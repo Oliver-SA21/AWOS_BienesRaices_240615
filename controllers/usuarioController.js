de8dc66f61cda416be34bd5fe3cc342df008552a
osanchez <osanchezarrioja@gmail.com>
2026-03-03 18:04:03 -0600

    PC-BienesRaices
    update: Se incorporaron los estilos de las página de registro

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,10 +1,10 @@
 const formularioLogin = (req, res) => {
-     res.render("auth/login");
+     res.render("auth/login", {pagina: "Inicia sesión"});
 }
 
 const formularioRegistro = (req,res) =>
 {
-    res.render("auth/registro");
+    res.render("auth/registro", {pagina: "Registate con nosotros :)"});
 }
 
 export { formularioLogin, formularioRegistro}
