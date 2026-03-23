1c9dd698e83977b69dc2edf2e58575f94d56f73a
osanchez <osanchezarrioja@gmail.com>
2026-03-22 22:46:22 -0600

    PC-BienesRaices
    update: Se actualizo el inicio del servidor para permitir la protección anti ataques CSRF, aún no se concluye la protección.

--- Index.js
+++ Index.js
@@ -1,11 +1,13 @@
 //console.log("Hola desde JS");
-import express from 'express'
-import usuarioRoutes from './routes/usuarioRoutes.js'
-import { connectDB } from './config/db.js';
+import express from 'express';
+import usuarioRoutes from './routes/usuarioRoutes.js';
+import { connectDB} from './config/db.js';
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import csurf from "@dr.pogodin/csurf";
 
 // Crea una instancia del contenedor web 
 const app = express();
-const PORT = process.env.PORT ?? 4000;
 
 // Habilitar el Template Engine (PUG)
 app.set("view engine", "pug");
@@ -16,10 +18,37 @@ app.use(express.static('public'))
 
 // Habilitar lectura de datos a través de las peticiones (REQUEST)
 app.use(express.urlencoded({extended: true}))
+// Activamos la opción para poder manipular Cookies - Almacenamiento en el cliente (navegador) 
+app.use(cookieParser());
+app.use(express.json());
+
+// Definimos el Middleware  - 
+app.use(session({
+    secret: process.env.SESSION_SECRET||"PC-BienesRaices_MATRICULA_csrf_secret",
+    resave: false,
+    saveUninitialized: false, 
+    cookie: {
+        httpOnly: true, 
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production"
+        }
+    }));
+
+//Habilitamos el mecanismo para protección de CSRF
+app.use(csurf())
+
+
+// Habilitar los tokes de CSRF para cualquier formulario
+app.use((req, res, next) =>
+{
+    res.locals.csrfToken = req.csrfToken();
+    next();
+});
 
 app.use("/auth", usuarioRoutes)
 await connectDB();
 
-app.listen(PORT, ()=> {
-    console.log(`El servidor esta iniciado en el puerto ${PORT}`)
+app.listen(process.env.PORT ?? 4000, ()=> {
+    console.log(`El servidor esta iniciado en el puerto ${process.env.PORT}`)
 }) 
+
